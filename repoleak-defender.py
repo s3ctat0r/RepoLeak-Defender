b7b4189f2bb7d92eb9213ae0708b7e94eb1ae3a2
Suprit Pandurangi <52566434+s3ctat0r@users.noreply.github.com>
2023-04-23 08:56:44 +0530

    Update repoleak-defender.py

--- repoleak-defender.py
+++ repoleak-defender.py
@@ -1,20 +1,106 @@
 print("""
 
-######                       #                               ######                                                   
-#     # ###### #####   ####  #       ######   ##   #    #    #     # ###### ###### ###### #    # #####  ###### #####  
-#     # #      #    # #    # #       #       #  #  #   #     #     # #      #      #      ##   # #    # #      #    # 
-######  #####  #    # #    # #       #####  #    # ####      #     # #####  #####  #####  # #  # #    # #####  #    # 
-#   #   #      #####  #    # #       #      ###### #  #      #     # #      #      #      #  # # #    # #      #####  
-#    #  #      #      #    # #       #      #    # #   #     #     # #      #      #      #   ## #    # #      #   #  
-#     # ###### #       ####  ####### ###### #    # #    #    ######  ###### #      ###### #    # #####  ###### #    # 
-                                                                                                                      
+██████  ███████ ██████   ██████  ██      ███████  █████  ██   ██     ██████  ███████ ███████ ███████ ███    ██ ██████  ███████ ██████  
+██   ██ ██      ██   ██ ██    ██ ██      ██      ██   ██ ██  ██      ██   ██ ██      ██      ██      ████   ██ ██   ██ ██      ██   ██ 
+██████  █████   ██████  ██    ██ ██      █████   ███████ █████       ██   ██ █████   █████   █████   ██ ██  ██ ██   ██ █████   ██████  
+██   ██ ██      ██      ██    ██ ██      ██      ██   ██ ██  ██      ██   ██ ██      ██      ██      ██  ██ ██ ██   ██ ██      ██   ██ 
+██   ██ ███████ ██       ██████  ███████ ███████ ██   ██ ██   ██     ██████  ███████ ██      ███████ ██   ████ ██████  ███████ ██   ██ 
+                                                                                                                                                                                                                                                           
 """)
 
 # Part-1 author@Vinayak Teli 
+# Libraries
+import sys
+import json
+import time
+import argparse
+import random
+import requests
+import csv
+from itertools import zip_longest
+from termcolor import colored
+from multiprocessing.dummy import Pool
 
+# Linking API
+GITHUB_API_URL = 'https://api.github.com'
 
+# PARSER
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--dorks", help="dorks file (required)")
+parser.add_argument("-k", "--keyword", help="search on a keyword instead of a list of dorks")
+parser.add_argument("-q", "--query", help="query (required or -q)")
+parser.add_argument("-qf", "--queryfile", help="query (required or -q)")
+parser.add_argument("-ri", "--recentlyindexed", action='store_true', help="sort results of queries from most recent first")
+parser.add_argument("-lb", "--limitbypass", action='store_true', help="increase requests per minute when using multiple tokens from UNIQUE accounts")
+parser.add_argument("-pf", "--patternfilter", action='store_true', help="filter out noise/patterns for test/example keys")
+parser.add_argument("-u", "--users", help="users to perform dork or keyword search on (comma separated).")
+parser.add_argument("-uf", "--userfile", help="file containing new line separated users")
+parser.add_argument("-org", "--organization",
+                    help="organization's GitHub name (required or -org if query not specified)")
+parser.add_argument("-t", "--token", help="your github token (required if token file not specififed)")
+parser.add_argument("-tf", "--tokenfile", help="file containing new line separated github tokens ")
+parser.add_argument("-e", "--threads", help="maximum n threads, default 1")
+parser.add_argument("-p", "--positiveresults", action='store_true', help="display positive results only")
+parser.add_argument("-o", "--output", help="output to file name (required or -o)")
 
+parser.parse_args()
+args = parser.parse_args()
 
+# DECLARE LISTS
+tokens_list = []
+dorks_list = []
+queries_list = []
+organizations_list = []
+users_list = []
+keywords_list = []
+
+# TOKEN ARGUMENT LOGIC
+if args.token:
+    tokens_list = args.token.split(',')
+
+if args.tokenfile:
+    with open(args.tokenfile) as f:
+        tokens_list = [i.strip() for i in f.read().splitlines() if i.strip()]
+
+# USER ARGUMENT LOGIC
+if args.users:
+    users_list = args.users.split(',')
+
+if args.userfile:
+    with open(args.userfile) as f:
+        users_list = [i.strip() for i in f.read().splitlines() if i.strip()]
+
+if args.query:
+    queries_list = args.query.split(',')
+
+if args.queryfile:
+    with open(args.queryfile) as f:
+        queries_list = [i.strip() for i in f.read().splitlines() if i.strip()]
+
+if args.patternfilter:
+    patternfilter = " -fake -example -test -XXXX -1234 -ABCD"
+
+if args.organization:
+    organizations_list = args.organization.split(',')
+
+if args.threads:
+    threads = int(args.threads)
+else:
+    threads = 1
+
+if args.dorks:
+    fp = open(args.dorks, 'r')
+    for line in fp:
+        dorks_list.append(line.strip())
+
+if args.keyword:
+    keywords_list = args.keyword.split(',')
+
+if not args.dorks and not args.keyword:
+    parser.error('dorks file or keyword is missing')
+
+# NUMBER OF REQUESTS PER MINUTE (TOKENS MUST BE UNIQUE)
+requests_per_minute = (len(tokens_list) * 30) - 1
 
 
 #Part-2  author@Suprit
@@ -156,6 +242,243 @@ if not args.keyword:
             url_dict[url] = 0
             
 # Part-3 author@Omkar
+# Updates
+stats_dict['n_total_urls'] = len(url_dict)
+print("""
+██    ██ ██████  ██████   █████  ████████ ███████ ███████ 
+██    ██ ██   ██ ██   ██ ██   ██    ██    ██      ██      
+██    ██ ██████  ██   ██ ███████    ██    █████   ███████ 
+██    ██ ██      ██   ██ ██   ██    ██    ██           ██ 
+ ██████  ██      ██████  ██   ██    ██    ███████ ███████ 
+                                                          
+""")
+sys.stdout.write(colored('[#] %d organizations found.\n' % len(organizations_list), 'cyan'))
+sys.stdout.write(colored('[#] %d users found.\n' % len(users_list), 'cyan'))
+sys.stdout.write(colored('[#] %d dorks found.\n' % len(dorks_list), 'cyan'))
+sys.stdout.write(colored('[#] %d keywords found.\n' % len(keywords_list), 'cyan'))
+sys.stdout.write(colored('[#] %d queries ran.\n' % len(queries_list), 'cyan'))
+sys.stdout.write(colored('[#] %d urls generated.\n' % len(url_dict), 'cyan'))
+sys.stdout.write(colored('[#] %d tokens being used.\n' % len(tokens_list), 'cyan'))
+sys.stdout.write(colored('[#] running %d threads.\n' % threads, 'cyan'))
+if args.limitbypass:
+    sys.stdout.write(colored('[#] %d requests per minute allowed\n' % requests_per_minute, 'cyan'))
+else:
+    sys.stdout.write(colored('[#] 29 requests per minute allowed\n', 'cyan'))
+print("")
+# Sleep
+time.sleep(1)
+
+# API search
+pool = Pool(threads)
+pool.map(api_search, url_dict)
+pool.close()
+pool.join()
+
+count = 0
+keyword_count = 0
+
+# Display result
+print("")
+print("""
+██████  ███████ ███████ ██    ██ ██   ████████ 
+██   ██ ██      ██      ██    ██ ██      ██    
+██████  █████   ███████ ██    ██ ██      ██    
+██   ██ ██           ██ ██    ██ ██      ██    
+██   ██ ███████ ███████  ██████  ███████ ██    
+                                              
+""")
+
+new_url_list = []
+result_number_list = []
+dork_name_list = []
+keyword_name_list = []
+user_list = []
+
+# Conditional output
+sys.stdout.write(colored('[+] SUCCESS | RESULTS RETURNED ', 'green'))
+print("")
+normal = sys.stdout.write(colored('[#] NEUTRAL | NO RESULTS RETURNED', 'yellow'))
+print("")
+failure = sys.stdout.write(colored('[-] FAILURE | RATE LIMITS OR API FAILURE ', 'red'))
+print("")
+
+# Result logic for queries
+for query in queries_list:
+    print("")
+    sys.stdout.write(colored('QUERY PROVIDED: %s' % (query), 'cyan'))
+    print("")
+    print("")
+
+    for url in results_dict[query]:
+
+        if url in url_results_dict:
+            if args.recentlyindexed:
+                new_url = url.replace('https://api.github.com/search/code',
+                                      'https://github.com/search') + '&s=indexed&type=Code&o=desc'
+            elif not args.recentlyindexed:
+                new_url = url.replace('https://api.github.com/search/code',
+                                      'https://github.com/search') + '&type=Code'
+            dork_name = dorks_list[count]
+            dork_info = 'DORK = ' + dork_name + ' | '
+            result_info = dork_info + new_url
+            if count < len(dorks_list)-1:
+                count = count + 1
+            else:
+                count = 0
+
+            if url_results_dict[url] == 0:
+                if args.positiveresults == False:
+                    result_number = url_results_dict[url]
+                    normal = sys.stdout.write(colored('[#] ', 'yellow'))
+                    sys.stdout.write(colored('(%d) ' % (result_number), 'cyan'))
+                    sys.stdout.write(colored('%s' % (result_info), 'white'))
+                    new_url_list.append(new_url)
+                    result_number_list.append(result_number)
+                    dork_name_list.append(dork_name)
+                    print('')
+
+            else:
+                result_number = url_results_dict[url]
+                success = sys.stdout.write(colored('[+] ', 'green'))
+                sys.stdout.write(colored('(%d) ' % (result_number), 'cyan'))
+                sys.stdout.write(colored('%s' % (result_info), 'white'))
+                new_url_list.append(new_url)
+                result_number_list.append(result_number)
+                dork_name_list.append(dork_name)
+                print('')
+
+        else:
+            failure = sys.stdout.write(colored('[-] ', 'red'))
+            sys.stdout.write(colored('%s' % new_url, 'white'))
+            count = count + 1
+            print('')
+
+
+# ADD Keyword to Output to DORKS & ARGS
+for user in users_list:
+    print("")
+    sys.stdout.write(colored('USER PROVIDED: %s' % (user), 'cyan'))
+    print("")
+
+    if args.keyword and not args.dorks:
+        for url in results_dict[user]:
+            if url in url_results_dict:
+                if args.recentlyindexed:
+                    new_url = url.replace('https://api.github.com/search/code',
+                                          'https://github.com/search') + '&s=indexed&type=Code&o=desc'
+                elif not args.recentlyindexed:
+                    new_url = url.replace('https://api.github.com/search/code',
+                                          'https://github.com/search') + '&type=Code'
+
+                keyword_name = keywords_list[keyword_count]
+                keyword_info = 'KEYWORD = ' + keyword_name + ' | '
+                result_info = keyword_info + new_url
+                if len(keywords_list) - 1 != keyword_count:
+                    keyword_count = keyword_count + 1
+                else:
+                    keyword_count = 0
+
+                if url_results_dict[url] == 0:
+                    if args.positiveresults == False:
+                        result_number = url_results_dict[url]
+                        normal = sys.stdout.write(colored('[#] ', 'yellow'))
+                        sys.stdout.write(colored('(%d) ' % (result_number), 'cyan'))
+                        sys.stdout.write(colored('%s' % (result_info), 'white'))
+                        new_url_list.append(new_url)
+                        result_number_list.append(result_number)
+                        keyword_name_list.append(keyword_name)
+                        user_list.append(user)
+                        print('')
+
+                else:
+                    result_number = url_results_dict[url]
+                    success = sys.stdout.write(colored('[+] ', 'green'))
+                    sys.stdout.write(colored('(%d) ' % (result_number), 'cyan'))
+                    sys.stdout.write(colored('%s' % (result_info), 'white'))
+                    new_url_list.append(new_url)
+                    result_number_list.append(result_number)
+                    keyword_name_list.append(keyword_name)
+                    user_list.append(user)
+                    print('')
+
+            else:
+                failure = sys.stdout.write(colored('[-] ', 'red'))
+                keyword_name = keywords_list[keyword_count]
+                sys.stdout.write(colored('No KEYWORD: %s found for %s' % (keyword_name, user), 'white'))
+                if len(keywords_list) - 1 != keyword_count:
+                    keyword_count = keyword_count + 1
+                else:
+                    keyword_count = 0
+                print('')
+
+
+    elif args.dorks:
+        count = 0
+        for url in results_dict[user]:
+            if url in url_results_dict:
+                if args.recentlyindexed:
+                    new_url = url.replace('https://api.github.com/search/code',
+                                          'https://github.com/search') + '&s=indexed&type=Code&o=desc'
+                elif not args.recentlyindexed:
+                    new_url = url.replace('https://api.github.com/search/code',
+                                          'https://github.com/search') + '&type=Code'
+                dork_name = dorks_list[count]
+
+                if args.keyword:
+                    keyword_name = keywords_list[keyword_count]
+                    dork_info = 'DORK = ' + dork_name + ' | KEYWORD = ' + keyword_name + ' | '
+                    result_info = dork_info + new_url
+                    if len(keywords_list) - 1 != keyword_count:
+                        keyword_count = keyword_count + 1
+                    else:
+                        keyword_count = 0
+                        count = count + 1
+
+                elif not args.keyword:
+                    count = count + 1
+                    dork_info = 'DORK = ' + dork_name + ' | '
+                    result_info = dork_info + new_url
+
+                if len(dorks_list) == count:
+                    count = 0
+
+                if url_results_dict[url] == 0:
+                    if args.positiveresults == False:
+                        result_number = url_results_dict[url]
+                        normal = sys.stdout.write(colored('[#] ', 'yellow'))
+                        sys.stdout.write(colored('(%d) ' % (result_number), 'cyan'))
+                        sys.stdout.write(colored('%s' % (result_info), 'white'))
+                        new_url_list.append(new_url)
+                        result_number_list.append(result_number)
+                        dork_name_list.append(dork_name)
+                        if args.keyword:
+                            keyword_name_list.append(keyword_name)
+                        user_list.append(user)
+                        print('')
+
+                else:
+                    result_number = url_results_dict[url]
+                    success = sys.stdout.write(colored('[+] ', 'green'))
+                    sys.stdout.write(colored('(%d) ' % (result_number), 'cyan'))
+                    sys.stdout.write(colored('%s' % (result_info), 'white'))
+                    new_url_list.append(new_url)
+                    result_number_list.append(result_number)
+                    dork_name_list.append(dork_name)
+                    if args.keyword:
+                        keyword_name_list.append(keyword_name)
+                    user_list.append(user)
+                    print('')
+
+            else:
+                failure = sys.stdout.write(colored('[-] ', 'red'))
+                sys.stdout.write(colored('%s' % new_url, 'white'))
+                if args.keyword:
+                    if len(keywords_list) - 1 != keyword_count:
+                        keyword_count = keyword_count + 1
+                count = count + 1
+                if len(dorks_list) == count:
+                    count = 0
+                print('')
 
 
 
